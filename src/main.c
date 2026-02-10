b0f13ade4ddcbd3a8217cfaf3d0d316fa287c678
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-10 15:48:36 +0100

    Changed the order of `main.c`

--- src/main.c
+++ src/main.c
@@ -3,9 +3,9 @@
 
 int main() {
     App_init();
-    // Looping until an error or `running = false`
     int error = App_loop();
-    if (error != APP_OK) fprintf(stderr, "ERROR: The app returned an error: %d\n", error);
     App_deinit();
+
+    if (error != APP_OK) fprintf(stderr, "ERROR: The app returned an error: %d\n", error);
     return error;
 }
