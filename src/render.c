fa540c2c038af34a4816b10bbc64769ff7b62a0b
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-10 16:28:44 +0100

    Added a simple UI

--- src/render.c
+++ src/render.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <ncurses.h>
+#include <stdarg.h>
 
 #include "render.h"
 #include "world.h"
@@ -10,10 +11,28 @@ void RenderLocal_drawTextureAt(uint32_t x, uint32_t y, Texture texture) {
     mvaddch(y, x*2+1, texture[1]);
 }
 
+void GCC_PRINTFLIKE(3, 4) RenderLocal_drawTextAt(uint32_t x, uint32_t y, const char* text, ...) {
+    va_list ap;
+    va_start(ap, text);
+    mvinch(y, x);
+    vw_printw(stdscr, text, ap);
+    va_end(ap);
+}
+
 void RenderLocal_drawCharacter() {
     RenderLocal_drawTextureAt(*Character_getX(), *Character_getY(), TEXTURE_CHARACTER);
 }
 
+void RenderLocal_drawUI() {
+    RenderLocal_drawTextAt(0, LINES-1, 
+            "HP: %d; POS: %dx, %dy;", 
+
+            *Character_getHp(), 
+            *Character_getX(), 
+            *Character_getY());
+}
+
 void Render_drawAll() {
     RenderLocal_drawCharacter();
+    RenderLocal_drawUI();
 }
