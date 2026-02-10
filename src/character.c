3b84a4683fcca547f0c3e2a92f39d56d060cd481
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-10 21:06:14 +0100

    Yeah this one as well please!

--- src/character.c
+++ src/character.c
@@ -1,7 +1,7 @@
 #include "character.h"
 
-Character GlobalCharacter = {0};
+static Character GlobalCharacter = {0};
 
-uint32_t* Character_getX() { return &GlobalCharacter.x; }
-uint32_t* Character_getY() { return &GlobalCharacter.y; }
+uint32_t* Character_getX()  { return &GlobalCharacter.x; }
+uint32_t* Character_getY()  { return &GlobalCharacter.y; }
 uint32_t* Character_getHp() { return &GlobalCharacter.hp; }
