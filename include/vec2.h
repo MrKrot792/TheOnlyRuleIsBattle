5f2aad175d7d0135a0d39d201c5edf287989d051
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-11 15:19:12 +0100

    Removing `inline` from `vec2.h`

--- include/vec2.h
+++ include/vec2.h
@@ -6,17 +6,17 @@ typedef struct {
     float y;
 } Vec2;
 
-inline Vec2 Vec2_create(float x, float y);
-inline Vec2 Vec2_zero(void);
+Vec2 Vec2_create(float x, float y);
+Vec2 Vec2_zero(void);
 
-inline Vec2 Vec2_add(Vec2 a, Vec2 b);
-inline Vec2 Vec2_sub(Vec2 a, Vec2 b);
-inline Vec2 Vec2_mul(Vec2 a, Vec2 b);
-inline Vec2 Vec2_div(Vec2 a, Vec2 b);
+Vec2 Vec2_add(Vec2 a, Vec2 b);
+Vec2 Vec2_sub(Vec2 a, Vec2 b);
+Vec2 Vec2_mul(Vec2 a, Vec2 b);
+Vec2 Vec2_div(Vec2 a, Vec2 b);
 
-inline Vec2 Vec2_scale(Vec2 a, float s);
-inline float Vec2_dot(Vec2 a, Vec2 b);
+Vec2 Vec2_scale(Vec2 a, float s);
+float Vec2_dot(Vec2 a, Vec2 b);
 
-inline float Vec2_length(Vec2 a);
-inline Vec2 Vec2_normalize(Vec2 a);
-inline Vec2 Vec2_negate(Vec2 a);
+float Vec2_length(Vec2 a);
+Vec2 Vec2_normalize(Vec2 a);
+Vec2 Vec2_negate(Vec2 a);
