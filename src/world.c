25b57511c8a26b9bf3336a0c546885753cff194c
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-11 16:43:29 +0100

    Oopsie, fixing an error in `world.c`!

--- src/world.c
+++ src/world.c
@@ -23,9 +23,9 @@ void World_deinit() { free(GlobalWorld.data); }
 Vec2 World_getSize() { return (Vec2){GlobalWorld.width, GlobalWorld.height}; }
 
 Block* World_getAt(Vec2 position) {
-    if (position.x < 0) App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.x < 0."); return NULL;
-    if (position.y < 0) App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.y < 0."); return NULL;
-    if (position.x > GlobalWorld.width-1) App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.x < World.width-1."); return NULL;
-    if (position.y < GlobalWorld.height-1) App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.y < World.height-1"); return NULL;
+    if (position.x < 0) { App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.x < 0."); return NULL; }
+    if (position.y < 0) { App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.y < 0."); return NULL; }
+    if (position.x > GlobalWorld.width-1) { App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.x < World.width-1."); return NULL; }
+    if (position.y < GlobalWorld.height-1) { App_breakWithMessage(APP_SEGFAULT, "From: World_getAt, position.y < World.height-1"); return NULL; }
     return &GlobalWorld.data[(uint32_t)position.y * GlobalWorld.width + (uint32_t)position.x];
 }
