d7ed7af9f8deb588ce058982fb31cd4a91c9c8e5
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-19 23:04:07 +0100

    Really simple system to visualize attack's state target

--- src/layers/game/attack.c
+++ src/layers/game/attack.c
@@ -10,12 +10,33 @@
 
 uint32_t move_index = 0;
 
+Vec2 target = {0};
+
 void attackUi() {
     Render_drawTextAt((Vec2){0, RENDER_REAL_HEIGHT-2}, 
             "Index: %d, name: %s", 
             move_index, Moves_getAt(move_index).move.name);
 
     Render_drawTextureAtCamera(Vec2_zero(), Texture_create(TEXTURE_ATTACK_OVERLAY));
+    Render_drawTextureAtCamera(target, Texture_create(TEXTURE_ATTACK_OVERLAY));
+}
+
+bool attackEvent(int ch) {
+    switch (ch) {
+        case 'a':
+            target.x--;
+            break;
+        case 'd':
+            target.x++;
+            break;
+        case 'w':
+            target.y--;
+            break;
+        case 's':
+            target.y++;
+            break;
+    }
+    return false;
 }
 
 Layer layerGameAttack(uint32_t move_index_internal) {
@@ -25,7 +46,7 @@ Layer layerGameAttack(uint32_t move_index_internal) {
     return (Layer){
         .init = InitFun_empty(),
         .deinit = SimpleFun_empty(),
-        .event = EventFun_empty(),
+        .event = EventFun_make(attackEvent),
         .render = SimpleFun_empty(),
         .update = SimpleFun_empty(),
         .ui = SimpleFun_make(attackUi),
