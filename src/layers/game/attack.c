515d2e6a1746537a9ccfc7e156b5cbcf66d4a9a6
Vladislav Korotianets <vkorotanec@gmail.com>
2026-02-26 12:49:59 +0100

    Made the attack preview italic

--- src/layers/game/attack.c
+++ src/layers/game/attack.c
@@ -8,6 +8,7 @@
 #include "render.h"
 #include "texture.h"
 #include "vec2.h"
+#include <ncurses.h>
 
 #define TEXTURE_ATTACK_OVERLAY (uint8_t[2]){'#', '#'}
 #define CENTERED(position) (Vec2){(int32_t)position.x + (RENDER_WIDTH/4), \
@@ -23,8 +24,8 @@ static void attackUi() {
             "Index: %d, name: %s", 
             move_index, Moves_getAt(move_index).move.name);
 
-    Render_drawTextureAt(CENTERED(Vec2_zero()), Texture_create(TEXTURE_ATTACK_OVERLAY));
-    Render_drawTextureAt(CENTERED(target), Texture_create(TEXTURE_ATTACK_OVERLAY));
+    Render_drawTextureAtDecorated(CENTERED(Vec2_zero()), Texture_create(TEXTURE_ATTACK_OVERLAY), A_ITALIC);
+    Render_drawTextureAtDecorated(CENTERED(target), Texture_create(TEXTURE_ATTACK_OVERLAY), A_ITALIC);
 }
 
 static bool attackEvent(int ch) {
